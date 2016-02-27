394a0a3311bec7a017ae5e4200f62fde10336a55
Philip John <phil@philipjohn.co.uk>
2016-02-27 18:14:57 +0000

    First test: inserting redirects

---
+++ tests/test-redirects.php
@@ -0,0 +1,41 @@
+<?php
+
+class WpcomLegacyRedirectsTest extends WP_UnitTestCase {
+
+	/**
+	 * Makes sure the foundational stuff is sorted so tests work
+	 */
+	function setup() {
+
+		// We need to trick the plugin into thinking it's run by WP-CLI
+		if ( ! defined( 'WP_CLI' ) ) {
+			define( 'WP_CLI', true );
+		}
+
+		// We need to trick the plugin into thinking we're in admin
+		if ( ! defined( 'WP_ADMIN' ) ) {
+			define( 'WP_ADMIN', true );
+		}
+
+	}
+
+	/**
+	 * Make sure redirects are added
+	 */
+	function test_insert_redirect() {
+
+		self::setup();
+
+		// Set our from/to URLs
+		$from = '/simple-redirect';
+		$to = 'http://example.com';
+
+		$redirect = WPCOM_Legacy_Redirector::insert_legacy_redirect( $from, $to );
+
+		$this->assertTrue( $redirect );
+
+	}
+
+
+}
+

67bae1f2b529ff18af764f74889cefb12d1ae753
Philip John <phil@philipjohn.co.uk>
2016-03-14 13:24:10 +0000

    Add simple tests for redirect URLs containing query strings

---
+++ tests/test-query-string-redirects.php
@@ -0,0 +1,62 @@
+<?php
+
+class WpcomLegacyQueryStringRedirectsTest extends WP_UnitTestCase {
+
+	/**
+	 * Makes sure the foundational stuff is sorted so tests work
+	 */
+	function setup() {
+
+		// We need to trick the plugin into thinking it's run by WP-CLI
+		if ( ! defined( 'WP_CLI' ) ) {
+			define( 'WP_CLI', true );
+		}
+
+		// We need to trick the plugin into thinking we're in admin
+		if ( ! defined( 'WP_ADMIN' ) ) {
+			define( 'WP_ADMIN', true );
+		}
+
+	}
+
+	/**
+	 * Make sure redirects are added
+	 *
+	 * The plugin should strip the query parameters and only store the URL path
+	 */
+	function test_insert_query_string_redirect() {
+
+		self::setup();
+
+		// Set our from/to URLs
+		$from = '/a-redirect?with=query-string';
+		$to = 'http://example.com';
+
+		$redirect = WPCOM_Legacy_Redirector::insert_legacy_redirect( $from, $to );
+
+		$this->assertTrue( $redirect );
+
+	}
+
+	/**
+	 * Make sure redirects are stored
+	 *
+	 * The plugin should strip any query params before checking for the redirect using
+	 * only the path from the input URL
+	 */
+	function test_get_query_string_redirect() {
+
+		self::setup();
+
+		$from = '/a-redirect?with=query-string';
+		$to = 'http://example.com';
+
+		$redirect = WPCOM_Legacy_Redirector::get_redirect_uri( $from );
+
+		$this->assertEquals( $redirect, $to );
+
+	}
+
+
+}
+

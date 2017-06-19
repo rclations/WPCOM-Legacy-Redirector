51f3b203e761c1b8349dce4590aaeae2cc01335c
Matthew Denton <matt@mdbitz.com>
2017-06-19 15:54:01 -0400

    Updated path '/' in hash test.

--- tests/test-redirects.php
+++ tests/test-redirects.php
@@ -77,7 +77,7 @@ class WpcomLegacyRedirectsTest extends WP_UnitTestCase {
                 // The plugin should strip the hash and only store the URL path.
                 '/hash-redirectA/#with-hash',
                 'http://example.com/',
-                '/hash-redirectA#with-hash/?utm_source=SDF',
+                '/hash-redirectA/#with-hash?utm_source=SDF',
                 'http://example.com/?utm_source=SDF'
             ),
 
